b17ae6dcfa4632516e16bba811ee4f1dce5ed582
George-Karpenko <g.r.karpenko@yandex.ru>
2024-04-13 09:40:30 +0300

    yml

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/subfolder/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/test_lead_hit/" : "/",
 });
